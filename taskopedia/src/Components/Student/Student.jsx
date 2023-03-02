c4db7bfceaf787228f2b155cfd331c8e87b7f977
YANG Jin <jyan863@aucklanduni.ac.nz>
2023-03-03 09:38:49 +1300

    convert Student, StudentReview and MainBody to class components.

--- taskopedia/src/Components/Student/Student.jsx
+++ taskopedia/src/Components/Student/Student.jsx
@@ -1,21 +1,28 @@
-const Student = (props) => {
-  return (
-    <div>
-      <div className="col-4 p-1">
-        <div className="row border">
-          <div className="col-2">
-            <img src={props.headshot} className="w-100" alt="initials"></img>
+import React from "react";
+class Student extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="col-4 p-1">
+          <div className="row border">
+            <div className="col-2">
+              <img
+                src={this.props.headshot}
+                className="w-100"
+                alt="initials"
+              ></img>
+            </div>
+            <div className="col-8">
+              {this.props.name}
+              <br />
+              Coding Experience: {this.props.experience} years
+            </div>
+            <div className="col-2">{this.props.children}</div>
           </div>
-          <div className="col-8">
-            {props.name}
-            <br />
-            Coding Experience: {props.experience} years
-          </div>
-          <div className="col-2">{props.children}</div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Student;
--- taskopedia/src/MainBody.jsx
+++ taskopedia/src/MainBody.jsx
@@ -1,46 +1,49 @@
 import Student from "./Components/Student/Student";
 import StudentReview from "./Components/Student/StudentReview";
+import React from "react";
 
-const MainBody = (props) => {
-  const whatWeWillLearn = "React JS";
-  const numberOfLectures = 3;
+class MainBody extends React.Component {
+  render() {
+    const whatWeWillLearn = "React JS";
+    const numberOfLectures = 3;
+    return (
+      <div style={{ minHeight: "70vh" }}>
+        <p>
+          In this course, we will learn {whatWeWillLearn} by building
+          TaskOpedia!
+          <br></br>
+          Total Lecture - {numberOfLectures}
+        </p>
 
-  return (
-    <div style={{ minHeight: "70vh" }}>
-      <p>
-        In this course, we will learn {whatWeWillLearn} by building TaskOpedia!
-        <br></br>
-        Total Lecture - {numberOfLectures}
-      </p>
+        <ul>
+          <li>Basic Foundation</li>
+          <li>Functional and Class Components</li>
+        </ul>
 
-      <ul>
-        <li>Basic Foundation</li>
-        <li>Functional and Class Components</li>
-      </ul>
+        <div className="container row">Students Enrolled</div>
+        <Student
+          experience={2}
+          name="Kris Walley"
+          headshot="https://api.lorem.space/image/face?w=150&h=153"
+        >
+          <StudentReview />
+        </Student>
+        <Student
+          experience={5}
+          name="Angel Patrice"
+          headshot="https://api.lorem.space/image/face?w=150&h=151"
+        >
+          <StudentReview></StudentReview>
+        </Student>
 
-      <div className="container row">Students Enrolled</div>
-      <Student
-        experience={2}
-        name="Kris Walley"
-        headshot="https://api.lorem.space/image/face?w=150&h=153"
-      >
-        <StudentReview />
-      </Student>
-      <Student
-        experience={5}
-        name="Angel Patrice"
-        headshot="https://api.lorem.space/image/face?w=150&h=151"
-      >
-        <StudentReview></StudentReview>
-      </Student>
-
-      <Student
-        experience={2}
-        name="Rene Parker"
-        headshot="https://api.lorem.space/image/face?w=150&h=150"
-      ></Student>
-    </div>
-  );
-};
+        <Student
+          experience={2}
+          name="Rene Parker"
+          headshot="https://api.lorem.space/image/face?w=150&h=150"
+        ></Student>
+      </div>
+    );
+  }
+}
 
 export default MainBody;
