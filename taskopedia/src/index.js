d7c1f07b4c2366a3c69c966594bd57630bf2ddb7
YANG Jin <jyan863@aucklanduni.ac.nz>
2023-03-02 16:31:28 +1300

    modify index.js to display header, mainbody and footer.

--- taskopedia/src/index.js
+++ taskopedia/src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+function Header() {
+  return (
+    <h1>REACT COURSE</h1>
+  )
+}
 
+function MainBody() {
+  return (
+    <div>
+      <p>
+        In this course, we will learn react js by building TaskOpedia!
+      </p>
+      <ul>
+        <li>Call Ben</li>
+        <li>Go to Walmart</li>
+      </ul>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      Happy Coding!
+    </footer>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
+  <div>
+    <Header/>
+    <MainBody />
+    <Footer/>
+  </div>
 );
-
-
