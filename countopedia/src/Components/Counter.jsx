1c88891a812500889eb183fcb98de4603490ce75
YANG Jin <jyan863@aucklanduni.ac.nz>
2023-03-03 12:06:08 +1300

    implement the random play and reset method by utilising method binding and arrow functions.

--- countopedia/src/Components/Counter.jsx
+++ countopedia/src/Components/Counter.jsx
@@ -1,37 +1,99 @@
 import React from "react";
+import attack from "../images/attack.png";
+import defend from "../images/defend.png";
 
 class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.handleAttack = this.handleAttack.bind(this);
     this.handleDefense = this.handleDefense.bind(this);
+    this.handleRandomPlay = this.handleRandomPlay.bind(this);
     this.state = {
       count: 0,
     };
   }
   handleAttack() {
     // alert("Attack clicked");
+
     this.setState((previousState) => {
-      return { count: previousState.count + 1 };
+      let newCount = previousState.count + Math.round(Math.random() * 10);
+      return { count: newCount };
     });
   }
 
   handleDefense() {
     // alert("Defend clicked");
     this.setState((previousState) => {
-      return { count: previousState.count - 1 };
+      let newCount = previousState.count - Math.round(Math.random() * 10);
+      return { count: newCount };
     });
+  }
+
+  handleReset = () => {
+    this.setState(() => {
+      return { count: 0 };
+    });
+  };
+
+  handleRandomPlay() {
+    // alert("Defend clicked");
+    let playMode = Math.round(Math.random());
+    if (playMode === 0) {
+      this.handleAttack();
+    } else {
+      this.handleDefense();
+    }
   }
   render() {
     return (
-      <div className="row text-white">
-        <h1>Counter:{this.state.count}</h1>
-        <button onClick={this.handleAttack} style={{ width: "200px" }}>
-          +1
-        </button>
-        <button onClick={this.handleDefense} style={{ width: "200px" }}>
-          -1
-        </button>
+      <div className="row text-white text-center">
+        <h1>Game Score:{this.state.count}</h1>
+        <p>You win at +10 points and lose at -10 points</p>
+        <p>Last Play: </p>
+        <h3>Game Status: </h3>
+        <div className="col-6 col-md-3 offset-md-3">
+          <img
+            src={attack}
+            onClick={this.handleAttack}
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "10px solid green",
+            }}
+            className="p-4 rounded"
+            alt="Attack"
+          ></img>
+        </div>
+
+        <div className="col-6 col-md-3 offset-md-3">
+          <img
+            src={defend}
+            onClick={this.handleDefense}
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "10px solid red",
+            }}
+            className="p-4 rounded"
+            alt="Defend"
+          ></img>
+        </div>
+
+        <div className="col-12 col-md-4 offset-md-4">
+          <button
+            className="btn btn-secondary w-100 mt-2"
+            onClick={this.handleRandomPlay}
+          >
+            Random Play
+          </button>
+          <br />
+          <button
+            className="btn btn-warning w-100 mt-2"
+            onClick={this.handleReset}
+          >
+            Reset
+          </button>
+        </div>
       </div>
     );
   }
--- countopedia/src/Layout/Header.jsx
+++ countopedia/src/Layout/Header.jsx
@@ -8,9 +8,7 @@ const Header = () => {
         style={{ height: "35px", verticalAlign: "top" }}
         alt="logo"
       ></img>
-      <span className="h2 pt-4 m-2 text-white-50">
-        React Course - CountOpedia
-      </span>
+      <span className="h2 pt-4 m-2 text-white-50">CountOpedia</span>
     </div>
   );
 };
