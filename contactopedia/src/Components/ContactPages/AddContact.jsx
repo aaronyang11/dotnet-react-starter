b08b147e17e086edc353fac11ad7390be5c8c645
YANG Jin <jyan863@aucklanduni.ac.nz>
2023-03-03 22:04:10 +1300

    add skeleton code to AddContact to check the logic works.

--- contactopedia/src/Components/ContactPages/AddContact.jsx
+++ contactopedia/src/Components/ContactPages/AddContact.jsx
@@ -1,30 +1,50 @@
-const AddContact = () => {
-  return (
-    <div className="border row text-white p-2">
-      <div className="col-12 text-white-50">Add a new contact</div>
-      <div className="col-12 col-md-4 p-2">
-        <input
-          className="form-control form-control-sm"
-          placeholder="Name..."
-        ></input>
+import React from "react";
+class AddContact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errorMessage: undefined,
+      successMessage: undefined,
+    };
+  }
+  handleAddContactFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleAddContact();
+  };
+  render() {
+    return (
+      <div className="border col-12 text-white p-2">
+        <form onSubmit={this.handleAddContactFormSubmit}>
+          <div className="row p-2">
+            <div className="col-12 text-white-50">Add a new contact</div>
+            <div className="col-12 col-md-4 p-2">
+              <input
+                className="form-control form-control-sm"
+                placeholder="Name..."
+              ></input>
+            </div>
+            <div className="col-12 col-md-4 p-2">
+              <input
+                className="form-control form-control-sm"
+                placeholder="Email..."
+              ></input>
+            </div>
+            <div className="col-12 col-md-4 p-2">
+              <input
+                className="form-control form-control-sm"
+                placeholder="Phone..."
+              ></input>
+            </div>
+            <div className="col-12 col-md-6 offset-md-3 p-2">
+              <button className="btn btn-primary btn-sm form-control">
+                Create
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-      <div className="col-12 col-md-4 p-2">
-        <input
-          className="form-control form-control-sm"
-          placeholder="Email..."
-        ></input>
-      </div>
-      <div className="col-12 col-md-4 p-2">
-        <input
-          className="form-control form-control-sm"
-          placeholder="Phone..."
-        ></input>
-      </div>
-      <div className="col-12 col-md-6 offset-md-3 p-2">
-        <button className="btn btn-primary btn-sm form-control">Create</button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default AddContact;
--- contactopedia/src/Components/ContactPages/ContactIndex.jsx
+++ contactopedia/src/Components/ContactPages/ContactIndex.jsx
@@ -36,34 +36,44 @@ class ContactIndex extends React.Component {
       ],
     };
   }
+
+  handleAddContact = () => {
+    alert("hello");
+  };
   render() {
     return (
       <div>
         <Header />
-        <div className="Container" style={{ minHeight: "85vh" }}>
-          <div className="row py-2">
-            <div className="col-4 offset-2">
+        <div className="container" style={{ minHeight: "85vh" }}>
+          <div className="row py-3">
+            <div className="col-4 offset-2 row">
               <AddRandomContact />
             </div>
             <div className="col-4 offset-2">
               <RemoveAllContact />
             </div>
             <div className="row py-2">
-              <AddContact />
+              <div className="col-8 offset-2 row">
+                <AddContact handleAddContact={this.handleAddContact} />
+              </div>
             </div>
             <div className="row py-2">
-              <FavoriteContacts
-                contacts={this.state.contactList.filter(
-                  (u) => u.isFavorite == true
-                )}
-              />
+              <div className="col-8 offset-2 row">
+                <FavoriteContacts
+                  contacts={this.state.contactList.filter(
+                    (u) => u.isFavorite == true
+                  )}
+                />
+              </div>
             </div>
             <div className="row py-2">
-              <GeneralContacts
-                contacts={this.state.contactList.filter(
-                  (u) => u.isFavorite == false
-                )}
-              />
+              <div className="col-8 offset-2 row">
+                <GeneralContacts
+                  contacts={this.state.contactList.filter(
+                    (u) => u.isFavorite == false
+                  )}
+                />
+              </div>
             </div>
           </div>
         </div>
