a09fe007984c9324f0e601f47974ab7dce32e8e7
YANG Jin <jyan863@aucklanduni.ac.nz>
2023-03-06 19:58:32 +1300

    initializa the project.

---
+++ cycleopedia/src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import logo from "../../images/react.png";
+
+const Header = () => {
+  return (
+    <div className="py-2 pt-2" style={{ borderBottom: "1px solid #777" }}>
+      <img
+        src={logo}
+        style={{ height: "35px", verticalAlign: "top" }}
+        alt="logo"
+      ></img>
+      <span className="h2 pt-4 m-2 text-white-50">CycleOpedia</span>
+    </div>
+  );
+};
+
+export default Header;
