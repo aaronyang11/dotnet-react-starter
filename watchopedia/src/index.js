9e0cca257745e3f8359fd6e931677fb150a0616f
YANG Jin <jyan863@aucklanduni.ac.nz>
2023-03-07 16:56:00 +1300

    implement the in/decrementCounter functionality using the setCounter returned by the useState() hook.

--- watchopedia/src/index.js
+++ watchopedia/src/index.js
@@ -1,10 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import Header from "./Components/Header"
+import Counter from "./Components/Counter"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <div>
     <Header />
+    <div className="p-2 m-2 row text-center">
+      <Counter />
+    </div>
   </div>
 )
