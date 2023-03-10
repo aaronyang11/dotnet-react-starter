860760bf64e9e0aed41b3690d93f1b4f556360ca
YANG Jin <jyan863@aucklanduni.ac.nz>
2023-03-11 11:29:01 +1300

    initialize project.

---
+++ travelopedia/src/layout/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import logo from "../images/react.png"
+
+function Header() {
+  return (
+    <div className="pt-3 pl-2">
+      {" "}
+      <img src={logo} style={{ height: "35px", verticalAlign: "top" }} alt="" />
+      <span className="h2 pt-4 text-white-50">React Course - TravelOPedia</span>
+    </div>
+  )
+}
+
+export default Header
