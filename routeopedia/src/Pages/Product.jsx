ae9938c9c50bdb40b6db439bd3bb03b5cf033f1c
YANG Jin <jyan863@aucklanduni.ac.nz>
2023-03-10 16:37:31 +1300

    utilise useState and Navigate to realize redirecting to another page.

--- routeopedia/src/Pages/Product.jsx
+++ routeopedia/src/Pages/Product.jsx
@@ -1,8 +1,12 @@
 import React from "react"
-import { useNavigate, Link } from "react-router-dom"
+import { useNavigate, Link, Navigate } from "react-router-dom"
+import { useState } from "react"
 
 function Product() {
   const navigate = useNavigate()
+  const [goToProduct, setGoToProduct] = useState(() => {
+    return false
+  })
   return (
     <div>
       Product
@@ -16,6 +20,10 @@ function Product() {
       <Link to="details/5">
         <button>Navigate to Product Detail - 5</button>
       </Link>
+      {goToProduct && <Navigate to="details/3" />}
+      <button onClick={() => setGoToProduct({ goToProduct: true })}>
+        Navigate to Product Detail - 3
+      </button>
     </div>
   )
 }
